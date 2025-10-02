1a88261947294249985c91ac65a2821c87cd5464
Manichandana1208 <chandanamani8247@gmail.com>
2025-10-02 12:34:28 -0500

    Third commit

---
+++ src/main/java/org/amazon/Calculator.java
@@ -0,0 +1,18 @@
+package org.amazon;
+
+public class Calculator {
+        public static void main(String[] args) {
+            int num1 = 20;
+            int num2 = 10;
+
+            int sum = num1 + num2;
+
+            int difference = num1 - num2;
+
+            System.out.println("First number: " + num1);
+            System.out.println("Second number: " + num2);
+            System.out.println("Sum: " + sum);
+            System.out.println("Subtraction: " + difference);
+        }
+    }
+
