16181c45022b094b63c388edd76f1a6f2f861398
Manichandana1208 <chandanamani8247@gmail.com>
2025-10-03 00:59:33 -0500

    I have added mul div reminder

--- src/main/java/org/amazon/Calculator.java
+++ src/main/java/org/amazon/Calculator.java
@@ -1,18 +1,34 @@
 package org.amazon;
 
+import java.util.Scanner;
+
 public class Calculator {
         public static void main(String[] args) {
-            int num1 = 20;
-            int num2 = 10;
+            Scanner sc = new Scanner(System.in);
+            System.out.println("Enter first number:");
+            int num1 = sc.nextInt();
+            System.out.println("Enter second number:");
+            int num2 = sc.nextInt();
 
             int sum = num1 + num2;
 
             int difference = num1 - num2;
 
+            int multiply = num1 * num2;
+            int division= num1/num2;
+            int reminder= num1 % num2;
+
             System.out.println("First number: " + num1);
             System.out.println("Second number: " + num2);
             System.out.println("Sum: " + sum);
             System.out.println("Subtraction: " + difference);
+            System.out.println("multiplication:"+multiply);
+            System.out.println("Division:" + num1/num2);
+            System.out.println("reminder:" + reminder);
+            System.out.println("Hi Sai");
+        }
+        public void sai(){
+            System.out.println("Hi Sai");
         }
     }
 
---
+++ src/main/java/org/amazon/day1/SumOfTwoNum.java
@@ -0,0 +1,13 @@
+package org.amazon.day1;
+
+import java.util.Scanner;
+
+public class SumOfTwoNum {
+  public static  void main(String[] args){
+      Scanner sc = new Scanner(System.in);
+      System.out.println("Enter a Number");
+      sc.nextInt();
+  }
+
+
+}
